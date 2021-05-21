0575e5281a91f8aab1bc9fa2f1963944eff47671
Javier González <doniagv@gmail.com>
2021-05-21 17:12:39 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Finance from "./components/Finance/Finance";
+
+function App() {
+  return (
+    <>
+      <div className="Hero">
+        <h1>Bakery Inventory System</h1>
+      </div>
+      <div>
+        <Finance />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Finance/Finance.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Finance.css";
+
+const Finance = () => {
+  return (
+    <>
+      <div className="finance-section">
+        <h2>Finance</h2>
+        <p>All your incomes and expenses in one place</p>
+      </div>
+    </>
+  );
+};
+
+export default Finance;
